1b5ccd8127eccb13fba597c065765041486ee54f
Steph <sakakabota@gmail.com>
2026-04-23 16:18:09 -0700

    organizing folders

--- code/calibration/calibration.ino
+++ code/calibration/calibration.ino
@@ -1,9 +1,20 @@
-void setup() {
-  // put your setup code here, to run once:
+const int potPin = 26;    // A0 
+const int pwm1 = 27;      // A1 
+const int pwm2 = 33;      // A2
 
+void setup() {
+  Serial.begin(115200);
+  pinMode(pwm1, OUTPUT);
+  pinMode(pwm2, OUTPUT);
+  
+  Serial.println("--- CALIBRATION MODE ---");
+  Serial.println("Use Maker Drive buttons to move actuator.");
+  Serial.println("Record the values for Full Retraction and Full Extension.");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  int rawValue = analogRead(potPin);
+  Serial.print("Current Potentiometer Value: ");
+  Serial.println(rawValue);
+  delay(100);
 }
